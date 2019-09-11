48eda4433a5d0a69b2a0e41b18fdce7323983d0d
thuongkenzin <nguyenhuuthuong97@gmail.com>
2019-09-11 16:33:26 +0700

    them receiver network

---
+++ app/src/main/java/com/example/downloader/NetworkChangeReceiver.java
@@ -0,0 +1,30 @@
+package com.example.downloader;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        boolean isConnected = isConnectingToInternet(context);
+        if(!isConnected){
+            Intent pauseAllDownload = new Intent(context,DownloadService.class);
+            pauseAllDownload.setAction(DownloadService.ACTION_PAUSE_ALL_DOWNLOAD);
+            context.startService(pauseAllDownload);
+        }
+    }
+    private boolean isConnectingToInternet(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netWorkInfo = connectivityManager.getActiveNetworkInfo();
+        if(netWorkInfo!=null && netWorkInfo.isConnected())
+            return true;
+        else
+            return false;
+    }
+}
